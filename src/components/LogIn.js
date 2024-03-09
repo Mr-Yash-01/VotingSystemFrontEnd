5f388ad19f01a39a6e2852487dcd3ee6854f1794
Mr-Yash-01 <mr.yashvadukiya.2003@gmail.com>
2024-03-10 00:04:09 +0530

    tp

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -56,7 +56,7 @@ function LogIn() {
           <h2>Voter Login</h2>
           <input
             type="text"
-            placeholder="Enter Voter ID"
+            placeholder="Enter Voter ID yash"
             value={voterId}
             onChange={(e) => setVoterId(e.target.value)}
           />
